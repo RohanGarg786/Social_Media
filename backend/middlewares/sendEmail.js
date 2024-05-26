8a15377d88294f218230466c1ef962ce8230d7eb
Rohan Garg <gargrohan026@gmail.com>
2024-05-26 18:57:00 +0530

    Added

--- backend/middlewares/sendEmail.js
+++ backend/middlewares/sendEmail.js
@@ -3,21 +3,21 @@ const nodeMailer =require("nodemailer")
 exports.sendEmail = async(options)=>{
     const transporter = nodeMailer.createTransport({
         //sending mail using gmail but due to security reasons gmail sometimes doesnt work 
-        // host:process.env.SMPT_HOST,
-        // port:process.env.SMPT_PORT,
-        // auth:{
-        //     user:process.env.SMPT_MAIL,
-        //     pass:process.env.SMPT_PASSWORD,
-        // },
-        // service:process.env.SMPT_SERVICE
+        host:process.env.SMPT_HOST,
+        port:process.env.SMPT_PORT,
+        auth:{
+            user:process.env.SMPT_MAIL,
+            pass:process.env.SMPT_PASSWORD,
+        },
+        service:process.env.SMPT_SERVICE
 
         //sending mail using mailtrap.com 
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: "f8da9038ceb8c1",
-          pass: "e3995562dea339"
-        }
+        // host: "sandbox.smtp.mailtrap.io",
+        // port: 2525,
+        // auth: {
+        //   user: "f8da9038ceb8c1",
+        //   pass: "e3995562dea339"
+        // }
     });
 
     const mailOptions = {
